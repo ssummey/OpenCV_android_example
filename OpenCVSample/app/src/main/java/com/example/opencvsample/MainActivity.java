06552090f4e00bb152ed82d51c479df5fb63a70f
ssummey <sdsummey@ncsu.edu>
2021-11-02 01:08:44 -0400

    Opencv installed in android project sucessfully

--- OpenCVSample/app/src/main/java/com/example/opencvsample/MainActivity.java
+++ OpenCVSample/app/src/main/java/com/example/opencvsample/MainActivity.java
@@ -3,35 +3,47 @@ package com.example.opencvsample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static String TAG = "MainActivity";
+    static
+    {
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Opencv installed successfully");
+        }
+        else {
+            Log.d(TAG, "Opencv is not installed");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
-                    Log.i(TAG, "OpenCV loaded successfully");
-                    mOpenCvCameraView.enableView();
-                } break;
-                default:
-                {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
-    }
+//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+//        @Override
+//        public void onManagerConnected(int status) {
+//            switch (status) {
+//                case LoaderCallbackInterface.SUCCESS:
+//                {
+//                    Log.i(TAG, "OpenCV loaded successfully");
+//                    mOpenCvCameraView.enableView();
+//                } break;
+//                default:
+//                {
+//                    super.onManagerConnected(status);
+//                } break;
+//            }
+//        }
+//    };
+//
+//    @Override
+//    public void onResume()
+//    {
+//        super.onResume();
+//        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+//    }
 }
